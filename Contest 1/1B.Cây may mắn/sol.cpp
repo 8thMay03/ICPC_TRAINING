1e578f68bb9d9adcd427b842a090bf0cae349683
8thMay03 <123087852+8thMay03@users.noreply.github.com>
2025-07-25 20:12:22 +0700

    Update sol.cpp

--- Contest 1/1B.Cây may mắn/sol.cpp
+++ Contest 1/1B.Cây may mắn/sol.cpp
@@ -38,7 +38,7 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cin >> n;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n - 1; i++) {
         int u, v, w;
         cin >> u >> v >> w;
         adj[u].push_back(v);
